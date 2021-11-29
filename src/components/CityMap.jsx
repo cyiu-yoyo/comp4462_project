58766401d6c3e2e5422c1ea50e77ae181ab0b3d8
NIE Fei <fnie9421@gmail.com>
2021-11-29 13:56:13 +0800

    add legend

--- src/components/CityMap.jsx
+++ src/components/CityMap.jsx
@@ -36,12 +36,44 @@ class CityMap extends React.Component {
                 height = 370 - margin.top - margin.bottom;
 
             d3.select("#citymap").selectAll("svg").remove();
+
             let svg = d3
                 .select("#citymap").append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            // legend
+            var linearGradient = svg.append("defs").append("linearGradient")
+                .attr("id", `linear-gradient`);
+
+            linearGradient
+                .attr("x1", "0%")
+                .attr("y1", "0%")
+                .attr("x2", "100%")
+                .attr("y2", "0%");
+
+            //Set the color for the start (0%)
+            linearGradient.append("stop")
+                .attr("offset", "0%")
+                .attr("stop-color", "PeachPuff");
+
+            //Set the color for the end (100%)
+            linearGradient.append("stop")
+                .attr("offset", "100%")
+                .attr("stop-color", "FireBrick");
+
+            svg.append("rect")
+                .attr("width", 90)
+                .attr("height", 15)
+                .style("fill", `url(#linear-gradient)`)
+                .attr("x", 10)
+                .attr("y", 325)
+
+            svg.append("text").text("Priority").attr("x", 10).attr("y", 320).attr("font-size", 13);
+            svg.append("text").text("low").attr("x", 10).attr("y", 350).attr("font-size", 13);
+            svg.append("text").text("high").attr("x", 80).attr("y", 350).attr("font-size", 13)
 
             const projection = d3.geoIdentity().reflectY(true).fitSize([width, height], geoData)
             const path = d3.geoPath(projection);
